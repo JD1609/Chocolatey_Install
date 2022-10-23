ee48334868c1ceb14b5daba14cfb0934b7e21b01
Jiří Dvořák <jiridvorak1998@seznam.cz>
2022-10-23 18:47:33 +0200

    Help link

--- Set ExecutionPolicy.bat
+++ Set ExecutionPolicy.bat
@@ -1,1 +1,2 @@
+REM https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-7.2
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Restricted -Force' -Verb RunAs }"
